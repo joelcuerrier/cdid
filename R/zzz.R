f059f8622a73465652a6446ff7127baef84064b3
Joel Cuerrier <joelcuerrier@Joels-MacBook-Pro.local>
2024-12-18 21:40:28 -0500

    18  decembre 2024

---
+++ R/zzz.R
@@ -0,0 +1,18 @@
+utils::globalVariables(c("year","att","att.se","post","group","x"))
+
+#' @title citation
+#'
+#' @description print the citation for the relevant paper
+#'
+#' @keywords internal
+citation <- function() {
+  cat("Reference: Christophe Bellégo, David Benatia, Vincent Dortet-Bernadet. \"The Chained Difference-in-Differences\" Journal of Econometrics. <https://arxiv.org/abs/2301.01085> \n")
+}
+
+
+
+
+
+
+
+
