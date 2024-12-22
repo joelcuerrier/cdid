9df096914cb3af3928d0534a96637b7fcbc8d80f
David Benatia <dbenatia@gmail.com>
2024-12-22 20:56:47 +0100

    Dec22

--- R/GMM_convert_result.R
+++ R/GMM_convert_result.R
@@ -190,7 +190,7 @@
       results$inffunc=sparse_matrix
       # results$inffunc=inffunc
       #results$t[results$group>results$t] <- results$t[results$group>results$t]+1
-      print(results)
+      #print(results)
 
 
     return(results)
